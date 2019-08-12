18dcc4f0399c5687ccbbc6d44130b56f7a8bafb6
Debashis Dip <nihan.dip@gmail.com>
2019-08-13 02:09:07 +0600

    ADD tests

--- apps/ticket/tests/test_views.py
+++ apps/ticket/tests/test_views.py
@@ -1,9 +1,12 @@
+import json
+
 import pytest
 from mixer.backend.django import mixer
 from rest_framework import status
-from ..views import TicketViewSet
 from rest_framework.test import force_authenticate, APIRequestFactory
-import json
+
+from ..models import Ticket
+from ..views import TicketViewSet, MessageListCreate
 
 pytestmark = pytest.mark.django_db
 
@@ -33,12 +36,74 @@ class TestTicketViewSet:
 
     def test_get_ticket_list(self, customer):
         mixer.cycle(10).blend("ticket.Ticket", created_by=customer.user)
+        mixer.cycle(5).blend("ticket.Ticket")
         factory = APIRequestFactory()
 
         request = factory.get("/")
         force_authenticate(request, customer.user)
         response = TicketViewSet.as_view({"get": "list"})(request)
+        response.render()
         content = json.loads(response.content)
         assert response.status_code == status.HTTP_200_OK, "Should retrieve the list"
 
+        assert content["count"] == 10, "Should have 10 results in total."
+
+    def test_send_message(self, customer):
+        ticket = mixer.blend("ticket.Ticket", created_by=customer.user)
+        factory = APIRequestFactory()
+        request = factory.post("/", data={"text": "lorem ipsum"})
+        force_authenticate(request, customer.user)
+        response = MessageListCreate.as_view()(request, ticket=ticket.id)
+        assert (
+            response.status_code == status.HTTP_201_CREATED
+        ), "Should create a message"
+
+    def test_send_message_in_others_ticket(self, customer):
+        ticket = mixer.blend("ticket.Ticket")
+        factory = APIRequestFactory()
+        request = factory.post("/", data={"text": "lorem ipsum"})
+        force_authenticate(request, customer.user)
+        response = MessageListCreate.as_view()(request, ticket=ticket.id)
+        assert (
+            response.status_code == status.HTTP_403_FORBIDDEN
+        ), "Should return permission denied error"
+
+    def test_send_message_ticket_closed(self, customer):
+        ticket = mixer.blend(
+            "ticket.Ticket", status=Ticket.CLOSED, created_by=customer.user
+        )
+        factory = APIRequestFactory()
+        request = factory.post("/", data={"text": "lorem ipsum"})
+        force_authenticate(request, customer.user)
+        response = MessageListCreate.as_view()(request, ticket=ticket.id)
+        assert (
+            response.status_code == status.HTTP_400_BAD_REQUEST
+        ), "Should return validation error"
+
+    def test_send_message_invalid_ticket(self, customer):
+        factory = APIRequestFactory()
+        request = factory.post("/", data={"text": "lorem ipsum"})
+        force_authenticate(request, customer.user)
+        response = MessageListCreate.as_view()(request, ticket="8783648723648723asdad")
+        assert (
+            response.status_code == status.HTTP_400_BAD_REQUEST
+        ), "Should return validation error"
+
+    def test_receive_text(self, customer):
+        ticket = mixer.blend("ticket.Ticket", created_by=customer.user)
+        mixer.cycle(10).blend(
+            "ticket.Message", ticket=ticket
+        )  # Message for original ticket
+        mixer.cycle(2).blend("ticket.Message")  # message for others
+
+        factory = APIRequestFactory()
+        request = factory.get("/")
+        force_authenticate(request, customer.user)
+        response = MessageListCreate.as_view()(request, ticket=ticket.id)
+        assert (
+            response.status_code == status.HTTP_200_OK
+        ), "Should receive a list of message"
+
+        response.render()
+        content = json.loads(response.content)
         assert content["count"] == 10, "Should have 10 results in total."
