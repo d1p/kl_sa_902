ede14744f33c8df2238e6727e92dd28ac9f6fd25
Debashis Dip <nihan.dip@gmail.com>
2019-10-22 20:12:41 +0600

    invoice admin add

--- apps/order/invoice/admin.py
+++ apps/order/invoice/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Transaction, Invoice, InvoiceItem
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "pt_transaction_id",
+        "pt_order_id",
+        "transaction_status",
+        "user",
+        "amount",
+        "created_at",
+    )
+    list_filter = ("created_at", "transaction_status")
+    search_fields = ("user", "id", "pt_order_id", "pt_transaction_id")
+
+
+class InvoiceItemInlineAdmin(admin.TabularInline):
+    model = InvoiceItem
+    fields = ("amount", "paid")
+
+
+@admin.register(Invoice)
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ("id", "order", "created_at")
+    search_fields = ("id", "order")
+    list_filter = ("created_at",)
