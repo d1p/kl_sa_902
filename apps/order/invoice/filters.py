260c2e03d21ba18fd659b65fc1e6a080824b1cf0
Debashis Dip <nihan.dip@gmail.com>
2019-11-19 02:17:34 +0600

    filter type change

--- apps/order/invoice/filters.py
+++ apps/order/invoice/filters.py
@@ -4,6 +4,8 @@ from apps.order.invoice.models import Invoice
 
 
 class InvoiceFilter(filters.FilterSet):
+    created_at = filters.DateTimeFromToRangeFilter(field_name="created_at")
+
     class Meta:
         model = Invoice
-        fields = {"created_at": ["range"]}
+        fields = ["created_at"]
