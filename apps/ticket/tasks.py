12a91f409ef7a75c329568b76b76a9c8cc9813f5
Debashis Dip <nihan.dip@gmail.com>
2020-01-13 20:42:13 +0600

    bling

--- apps/ticket/tasks.py
+++ apps/ticket/tasks.py
@@ -11,23 +11,22 @@ def send_new_ticket_notification():
 @app.task
 def send_message_notification(message_id: int):
     message = RestaurantMessage.objects.get(id=message_id)
-    if message.sender.is_staff is False and message.sender.is_superuser is False:
-        # This is a real user, send a push notification to the users device.
-        data = {
-            "notification_id": 0,
-            "notification_action": "NEW_MESSAGE",
-            "id": message.id,
-            "sender": message.sender.id,
-            "receiver": message.ticket.created_by.id,
-            "sender_name": message.sender.name,
-            "sender_profile_picture": message.sender.profile_picture.url,
-            "text": message.text,
-        }
-        print(data)
-        send_push_notification(
-            user=message.ticket.created_by,
-            title=message.sender.name,
-            body=message.text,
-            data=data,
-        )
-        print("OK!")
+    # This is a real user, send a push notification to the users device.
+    data = {
+        "notification_id": 0,
+        "notification_action": "NEW_MESSAGE",
+        "id": message.id,
+        "sender": message.sender.id,
+        "receiver": message.ticket.created_by.id,
+        "sender_name": message.sender.name,
+        "sender_profile_picture": message.sender.profile_picture.url,
+        "text": message.text,
+    }
+    print(data)
+    send_push_notification(
+        user=message.ticket.created_by,
+        title=message.sender.name,
+        body=message.text,
+        data=data,
+    )
+    print("OK!")
