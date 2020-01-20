8b45d3108dae1c81a435bda939d350272c6a84f2
Debashis Dip <nihan.dip@gmail.com>
2020-01-20 19:45:00 +0600

    phone number min length 9 to 10

--- apps/account/models.py
+++ apps/account/models.py
@@ -14,7 +14,7 @@ from .managers import CustomUserManager
 from .types import ProfileType
 
 _PHONE_REGEX = RegexValidator(
-    regex=r"^\+?1?\d{9,15}$",
+    regex=r"^\+?1?\d{10,15}$",
     message=_(
         "Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."
     ),
