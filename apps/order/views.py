8e48fb602dcb03a83f85d6a53ae751365f43b724
Debashis Dip <nihan.dip@gmail.com>
2020-04-28 14:04:32 +0600

    add leave message

--- apps/order/views.py
+++ apps/order/views.py
@@ -243,7 +243,7 @@ class OrderViewSet(
             )
             return Response(status=status.HTTP_204_NO_CONTENT)
         else:
-            return Response({"status": "failed"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"status": "Please complete the order first."}, status=status.HTTP_400_BAD_REQUEST)
 
     @action(detail=True, methods=["POST"])
     def confirm_current_items(self, request, pk):
