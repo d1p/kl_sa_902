03be021785b4eef0f0e402fe8ea015208ff74db8
Debashis Dip <nihan.dip@gmail.com>
2020-05-22 01:05:07 +0600

    time fix

--- conf/settings.py
+++ conf/settings.py
@@ -141,7 +141,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 5,
-    "DATETIME_FORMAT": "%Y-%m-%d %H:%m:%S",
+    "DATETIME_FORMAT": "%Y-%m-%d %H:%M:%S",
     "DATETIME_INPUT_FORMATS": ["iso-8601", "%Y-%m-%d %H:%M:%S"],
 }
 
