6fa40a276b8e63912a6c1c4186865c939646bce1
Debashis Dip <nihan.dip@gmail.com>
2020-06-28 10:29:58 +0600

    add transaction block

--- apps/order/invoice/utils.py
+++ apps/order/invoice/utils.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 
 import requests
 from django.conf import settings
+from django.db import transaction
 
 from apps.account.restaurant.models import Restaurant
 from apps.order.invoice.models import Invoice
@@ -36,34 +37,34 @@ def capture_transaction(transaction_id, amount):
 
 
 def process_new_completed_order_earning(order: Order):
-    restaurant: Restaurant = order.restaurant.restaurant
+    with transaction.atomic():
+        restaurant: Restaurant = order.restaurant.restaurant
+        total = Decimal(0.0)
+        try:
+            invoice = Invoice.objects.get(order=order)
+        except Invoice.DoesNotExist:
+            raise ValueError("Invoice does not exists")
 
-    total = Decimal(0.0)
-    try:
-        invoice = Invoice.objects.get(order=order)
-    except Invoice.DoesNotExist:
-        raise ValueError("Invoice does not exists")
+        for invoice_item in invoice.invoice_items.all():
+            total += invoice_item.amount
 
-    for invoice_item in invoice.invoice_items.all():
-        total += invoice_item.amount
+        app_earning = (total / Decimal(100)) * invoice.order_cut
+        restaurant_earning = total - app_earning
 
-    app_earning = (total / Decimal(100)) * invoice.order_cut
-    restaurant_earning = total - app_earning
+        if order.order_type is OrderType.PICK_UP:
+            restaurant.app_pickup_earning += app_earning
+            restaurant.pickup_earning += restaurant_earning
+        elif order.order_type is OrderType.IN_HOUSE:
+            restaurant.app_inhouse_earning += app_earning
+            restaurant.inhouse_earning += restaurant_earning
 
-    if order.order_type is OrderType.PICK_UP:
-        restaurant.app_pickup_earning += app_earning
-        restaurant.pickup_earning += restaurant_earning
-    elif order.order_type is OrderType.IN_HOUSE:
-        restaurant.app_inhouse_earning += app_earning
-        restaurant.inhouse_earning += restaurant_earning
+        restaurant.total_earning += restaurant_earning
+        restaurant.app_total_earning += app_earning
 
-    restaurant.total_earning += restaurant_earning
-    restaurant.app_total_earning += app_earning
+        restaurant.total += total
 
-    restaurant.total += total
+        restaurant.save()
 
-    restaurant.save()
-
-    invoice.app_earning = app_earning
-    invoice.restaurant_earning = restaurant_earning
-    invoice.save()
+        invoice.app_earning = app_earning
+        invoice.restaurant_earning = restaurant_earning
+        invoice.save()
